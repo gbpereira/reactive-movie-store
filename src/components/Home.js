8ace01a924b8d514a99fce015dd67b7236c96588
Guilherme Pereira <gbp.guilherme@gmail.com>
2021-07-09 00:48:44 -0300

    adiciona botão load more

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
+import Button from './Button';
 
 // hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -18,9 +19,9 @@ import NoImage from '../images/no_image.jpg';
 import { property } from 'lodash';
 
 const Home = () => {
-  const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
+  const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore } = useHomeFetch();
 
-  console.log(state);
+  if (error) return <div>Something went wrong...</div>;
 
   return (
     <>
@@ -46,7 +47,10 @@ const Home = () => {
               />
           ))}
         </Grid>
-        <Spinner />
+        {loading && <Spinner />}
+        {state.page < state.total_pages && !loading && (
+          <Button text='Load more' callback={() => setIsLoadingMore(true)} />
+        )}
     </>
   );
 }
