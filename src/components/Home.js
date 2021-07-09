a353b6db8148b76137fb91d9636fbec727019511
Guilherme Pereira <gbp.guilherme@gmail.com>
2021-07-08 23:56:41 -0300

    adiciona spinner

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
+import Spinner from './Spinner';
 
 // hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -45,6 +46,7 @@ const Home = () => {
               />
           ))}
         </Grid>
+        <Spinner />
     </>
   );
 }
